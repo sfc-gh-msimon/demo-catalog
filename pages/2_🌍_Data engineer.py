c031238a4cadaf7716255c10b499cb2c94db9849
Maxime Simon <maxime.simon@snowflake.com>
2022-12-14 10:54:19 +0100

    changed page structure, added first demo

---
+++ pages/2_🌍_Data engineer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Data engineer", page_icon="🌍", layout="wide")
+st.header("Data engineer")
+
+st.markdown("""##### Administration de base de données, DevOps et création de tableaux de bords chez Citibike """)
+
+st.write("Administration de base de données, DevOps et création de tableaux de bords chez Citibike")
+st.write("[Video](https://drive.google.com/file/d/15nViolkbysKCVbDSV1JcmebOKb7XswLB/view?usp=share_link)")
+st.write("[Github](https://github.com/snowflakecorp/citibike/wiki/V4_1-Welcome-to-Citibike)")
---
+++ pages/3_⚛️_Data scientist.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Data scientist", page_icon="⚛️", layout="wide")
+st.header("Data scientist")
+
+st.markdown("""##### Movies analysis """)
+
+st.write("Movies analysis on Python")
+st.write("[Video](https://drive.google.com/file/d/15uRB3nFhXkOHJ6_cEmP3VJwWdke9rgAB/view?usp=share_link)")
+st.write("[Github](https://github.com/Snowflake-Labs/snowpark-python-examples/tree/main/Snowpark101)")
