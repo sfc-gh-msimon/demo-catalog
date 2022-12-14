3482d40a8706d5f614641334a2dfd52a7d88eb3a
Maxime Simon <maxime.simon@snowflake.com>
2022-12-14 12:16:34 +0100

    mise en page

--- 1_🏠catalog.py
+++ 1_🏠catalog.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Snowflake Demo Catalog",
-    page_icon="🏠",
-)
+st.set_page_config(page_title="Snowflake Demo Catalog",page_icon="🏠",layout="wide")
 
 st.title("Snowflake Demo Catalog ❄️")
 
@@ -13,9 +10,9 @@ st.markdown(
 """
 #### Les démos sont classées par persona et par industrie.
 #### Chaque démo est présentée en vidéo, accompagnée de scripts SQL et/ou Python quand c'est possible.
-- ##### 📄 Analyste : Utilise Snowflake pour répondre à des questions. Intéressé par comment Snowflake résout des problèmes émanant des métiers.
-- ##### 🌍 Data Engineer : Utilise Snowflake pour préparer des données afin qu'elles puissent être analysées. Intéressé par la facilité d'utilisation et l'optimisation de performance.
-- ##### ⚛️ Data Scientist : Utilise Snowflake pour créer des modèles analytiques et prédictifs. Intéressé par l'accès centralisé à toutes les données nécessaires, la gouvernance des données et la création de modèles en plusieurs languages.
-""")
+- 📄 Analyste : Utilise Snowflake pour répondre à des questions. Intéressé par comment Snowflake résout des problèmes émanant des métiers.
+- 🌍 Data Engineer : Utilise Snowflake pour préparer des données afin qu'elles puissent être analysées. Intéressé par la facilité d'utilisation et l'optimisation de performance.
+- ⚛️ Data Scientist : Utilise Snowflake pour créer des modèles analytiques et prédictifs. Intéressé par l'accès centralisé à toutes les données nécessaires, la gouvernance des données et la création de modèles en plusieurs languages.
 
-st.write("Git repo: https://github.com/sfc-gh-msimon/demo-catalog")
+[Github](https://github.com/sfc-gh-msimon/demo-catalog)
+""")
