9ab2d678b93f91eb040c4545ced8f9d6dc9b2f71
Maxime Simon <maxime.simon@snowflake.com>
2022-12-14 17:06:56 +0100

    emojis

--- pages/3_⚛️_scientist.py
+++ pages/3_⚛️_scientist.py
@@ -10,16 +10,16 @@ st.markdown(
 - [Video](https://drive.google.com/file/d/15uRB3nFhXkOHJ6_cEmP3VJwWdke9rgAB/view?usp=share_link)
 - [Github](https://github.com/Snowflake-Labs/snowpark-python-examples/tree/main/Snowpark101)
 
-### Immobilier
+### 🏡 Immobilier
 - [Video](https://drive.google.com/file/d/16U1eTSK-CRljZdAmJiQNhUh9hu7XDn9d/view?usp=share_link)
 
-### 🧬 Master data management : entity matching avec TF-IDF
+### 👩‍👩‍👧‍👦 Master data management : entity matching avec TF-IDF
 - [Video](https://drive.google.com/file/d/16ahsm-00jimRr6A5Un_LfKh9wyMnU6FM/view?usp=share_link)
 """)
 
 st.markdown(
 """
 # Retail
-### 💃🏽 Détection de fraudes 
+### 🥷🏻 Détection de fraudes 
 - [Video](https://drive.google.com/file/d/15vGZxInFU0Fy8jOLrSZOl5PjxaRZxocB/view?usp=share_link)
 """)
