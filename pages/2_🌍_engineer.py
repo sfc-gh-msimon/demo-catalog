a686fef78a4efb91625a2b67c55641e20d575a74
Maxime Simon <maxime.simon@snowflake.com>
2023-01-09 23:11:34 +0100

    used magic command

--- pages/2_🌍_engineer.py
+++ pages/2_🌍_engineer.py
@@ -3,17 +3,21 @@ import streamlit as st
 st.set_page_config(page_title="Data engineer", page_icon="🌍", layout="wide")
 st.header("Data engineer")
 
-st.markdown(
+
 """
 # Toutes industries
 #### 🔧 Join elimination
 - [Video 3 min](https://drive.google.com/file/d/16qU_2BrwPqBnIpZ6J0NtZDKCRqWLBn6B/view?usp=share_link)
+"""
 
 
+"""
 ### 🏎 Performance optimization
 - [Video 18 min](https://drive.google.com/file/d/16vgQ-5IkT2bPaxvnqzEOs9kbc_tw-eQt/view?usp=share_link)
+"""
 
 
+"""
 ### 🚲 Administration de base de données, DevOps et création de tableaux de bords chez Citibike
 1. Administration de base de données
 2. DevOps : clonage de base de données et masking de données pour les développeurs
@@ -21,12 +25,19 @@ st.markdown(
 - [Video 24 min](https://drive.google.com/file/d/15nViolkbysKCVbDSV1JcmebOKb7XswLB/view?usp=share_link)
 - [Github](https://github.com/snowflakecorp/citibike/wiki/V4_1-Welcome-to-Citibike)
 - [Quickstart Zero to Snowflake](https://quickstarts.snowflake.com/guide/getting_started_with_snowflake/)
+"""
 
 
-""")
+
+"""
+### 🤝 Créez votre data clean room sur Snowflake 
+1. Introduction aux data clean rooms
+2. Les composants uniques de Snowflake pour créer des data clean rooms : Data sharing, row access policies, stored procedures, streams and tasks.
+3. Créer une data clean room entre deux organisations
+"""
 st.video('https://youtu.be/_0wE0p4vglo')
 
-st.markdown(
+
 """
 # Healthcare et Life sciences
 ### 🧬 Genomics 
@@ -34,4 +45,4 @@ st.markdown(
 2. Joindre des allèles avec des études stockées dans deux tables différentes, afin d'identifier quelles allèles ont potentiellement un effet délétère
 3. Accélérer des requêtes sur de grandes tables grace à l'architecture des micro-partitions de Snowflake (pruning) et au clustering
 - [Video 14 min](https://drive.google.com/file/d/154GzuSZBXUjtSPs_e6LFAWwA9Hg3NoVr/view?usp=share_link)
-""")
+"""
