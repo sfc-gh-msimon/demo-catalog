22eda6b17527edd9109c869729f519f3963ce19b
Maxime Simon <maxime.simon@snowflake.com>
2023-01-11 10:37:31 +0100

    added snowpark 101

--- pages/3_⚛️_Data_Scientist.py
+++ pages/3_⚛️_Data_Scientist.py
@@ -3,21 +3,64 @@ from annotated_text import annotated_text
 
 st.set_page_config(page_title="Data scientist", page_icon="⚛️",layout="wide")
 
-"""
-# Toutes industries
-#### 🎥 Movies analysis
-- [Video 10 min](https://drive.google.com/file/d/15uRB3nFhXkOHJ6_cEmP3VJwWdke9rgAB/view?usp=share_link)
-- [Github](https://github.com/Snowflake-Labs/snowpark-python-examples/tree/main/Snowpark101)
 
-### 🏡 Immobilier
-- [Video 15 min](https://drive.google.com/file/d/16U1eTSK-CRljZdAmJiQNhUh9hu7XDn9d/view?usp=share_link)
+'### 🎥 Snowpark 101 - Analyse sur les films'
+annotated_text(
+    ("Any",                 "Industrie",        "#8ef","#000"),
+    ("Snowpark python",     "Feature",          "#fea","#000"),
+    ("Jupyter notebook",    "Outil",            "#afa","#000")
+)
+'''
+[Github](https://github.com/Snowflake-Labs/snowpark-python-examples/tree/main/Snowpark101)
+1. Utiliser un Jupyter Notebook pour accéder à Snowflake avec Snowpark python.
+2. Créer des fonctions en local et les exécuter sur Snowflake
+3. Créer une régression linéaire pour préduire le nombre de films produits annuellement
+'''
+col1, col2, col3 = st.columns(3)
+with col2:
+   st.video('https://youtu.be/_peXDwdEpXA')
 
-### 👩‍👩‍👧‍👦 Master data management : entity matching avec TF-IDF
-- [Video 16 min](https://drive.google.com/file/d/16ahsm-00jimRr6A5Un_LfKh9wyMnU6FM/view?usp=share_link)
-"""
 
-"""
-# Retail
-### 🥷🏻 Détection de fraudes 
-- [Video 16 min](https://drive.google.com/file/d/15vGZxInFU0Fy8jOLrSZOl5PjxaRZxocB/view?usp=share_link)
-"""
+
+'### 🏡 Immobilier'
+annotated_text(
+    ("Any",                  "Industrie",        "#8ef","#000"),
+    ("Snowpark python",      "Feature",          "#fea","#000"),
+    ("Jupyter notebook",     "Outil",            "#afa","#000")
+)
+'''
+
+'''
+col1, col2, col3 = st.columns(3)
+with col2:
+   st.video('https://youtu.be/rMXLpGICcyE')
+
+
+
+'### 👩‍👩‍👧‍👦 Master data management : entity matching avec TF-IDF'
+annotated_text(
+    ("Any",             "Industrie",        "#8ef","#000"),
+    ("Snowpark python",      "Feature",          "#fea","#000"),
+    ("Jupyter notebook",       "Outil",            "#afa","#000")
+)
+'''
+
+'''
+col1, col2, col3 = st.columns(3)
+with col2:
+   st.video('https://youtu.be/rMXLpGICcyE')
+
+
+
+'### 🥷🏻 Détection de fraudes '
+annotated_text(
+    ("Any",             "Industrie",        "#8ef","#000"),
+    ("Snowpark python",      "Feature",          "#fea","#000"),
+    ("Jupyter notebook",       "Outil",            "#afa","#000")
+)
+'''
+
+'''
+col1, col2, col3 = st.columns(3)
+with col2:
+   st.video('https://youtu.be/rMXLpGICcyE')
